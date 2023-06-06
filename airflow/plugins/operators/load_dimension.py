1d482b2f7853ccecc33df229ff8332d0c4fd2e48
Tung Nguyen <tungnt@Tungs-Mac-mini.local>
2023-06-06 16:20:00 +0700

    update 2023-06-06

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -14,6 +14,7 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id='',
                  table='',
                  query='',
+                 append_only = '',
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
@@ -23,10 +24,14 @@ class LoadDimensionOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.query = query
+        self.append_only = append_only
 
     def execute(self, context):
         self.log.info('Start LoadDimensionOperator')
         redshift_hook = PostgresHook(self.redshift_conn_id)
+        if not self.append_only:
+            self.log.info('Truncate table')
+            redshift_hook.run(f"DELECT FROM {self.table}")
         run_query = """
             INSERT INTO {table} {query};
         """.format(table = self.table, query = self.query)
